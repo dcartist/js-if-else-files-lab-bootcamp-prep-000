742f2ffb98749a1976b225bab331ef741e1b865d
dcartist <pbannerman13@gmail.com>
2018-05-15 16:42:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name==="Susan"){
 } else {
   console.log("Expected the name to be Susan")
 }
-if (height === "70"){
+if (height === "74"){
   console.log("The height is correct")
 } else {
   console.log("Expected to be 74")
